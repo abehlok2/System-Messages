667abf7ae4830d3bb6d95e7ed82e0a86e8148b5e
abehlok2 <alexbehlok@gmail.com>
2023-10-21 16:18:18 -0400

    aider: Added labels above the input and output areas, added a status bar at the bottom, and improved the layout and styling of the widgets.
    # Aider chat conversation:
    
    ASSISTANT: To create a high-quality graphical user interface (GUI) using the PyQt5 library, we will need to modify the `smgmain.py` file. We will add more widgets to make the interface more interactive and user-friendly.
    
    Here are the steps we will follow:
    
    1. Add a label above the input area to guide the user.
    2. Add a label above the output area to indicate where the system message will be displayed.
    3. Add a status bar at the bottom of the window to display the status of the system message generation.
    4. Improve the layout and styling of the widgets for a better user experience.
    
    Here are the changes we need to make:
    
    ```python
    smgmain.py
    <<<<<<< HEAD
    from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QPushButton, QVBoxLayout
    =======
    from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QPushButton, QVBoxLayout, QLabel, QStatusBar
    >>>>>>> updated
    ```
    
    ```python
    smgmain.py
    <<<<<<< HEAD
        layout = QVBoxLayout()
    
        global input_area, output_area  # Make these global to access in `process_input`
    
        input_area = QTextEdit()
        layout.addWidget(input_area)
    =======
        layout = QVBoxLayout()
    
        global input_area, output_area, status_bar  # Make these global to access in `process_input`
    
        input_label = QLabel("Enter your request:")
        layout.addWidget(input_label)
    
        input_area = QTextEdit()
        layout.addWidget(input_area)
    >>>>>>> updated
    ```
    
    ```python
    smgmain.py
    <<<<<<< HEAD
        submit_button = QPushButton("Submit")
        submit_button.clicked.connect(process_input)  # Connect button to function
        layout.addWidget(submit_button)
    
        output_area = QTextEdit()
        layout.addWidget(output_area)
    =======
        submit_button = QPushButton("Generate System Message")
        submit_button.clicked.connect(process_input)  # Connect button to function
        layout.addWidget(submit_button)
    
        output_label = QLabel("System Message:")
        layout.addWidget(output_label)
    
        output_area = QTextEdit()
        layout.addWidget(output_area)
    
        status_bar = QStatusBar()
        layout.addWidget(status_bar)
    >>>>>>> updated
    ```
    
    ```python
    smgmain.py
    <<<<<<< HEAD
        output_message = f"Processed: {user_input}"  # Placeholder
        output_area.setPlainText(output_message)
    =======
        # output_message = f"Processed: {user_input}"  # Placeholder
        output_message = generate_system_message(user_input)
        output_area.setPlainText(output_message)
        status_bar.showMessage("System message generated successfully.")
    >>>>>>> updated
    ```
    
    These changes will create a more interactive and user-friendly GUI for your application.

--- smgmain.py
+++ smgmain.py
@@ -1,5 +1,5 @@
 from SystemMessageGenerator import generate_system_message
-from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QPushButton, QVBoxLayout, QLabel, QStatusBar
 # Import the function from your script that takes the input prompt and returns the system message.
 # from SystemMessageGenerator import your_function
 
@@ -10,8 +10,10 @@ def process_input():
     # output_message = your_function(user_input)
     output_message = generate_system_message(user_input)
 
-    output_message = f"Processed: {user_input}"  # Placeholder
+    # output_message = f"Processed: {user_input}"  # Placeholder
+    output_message = generate_system_message(user_input)
     output_area.setPlainText(output_message)
+    status_bar.showMessage("System message generated successfully.")
 
 
 def init_pyqt():
@@ -22,17 +24,26 @@ def init_pyqt():
 
     layout = QVBoxLayout()
 
-    global input_area, output_area  # Make these global to access in `process_input`
+    global input_area, output_area, status_bar  # Make these global to access in `process_input`
+
+    input_label = QLabel("Enter your request:")
+    layout.addWidget(input_label)
 
     input_area = QTextEdit()
     layout.addWidget(input_area)
 
-    submit_button = QPushButton("Submit")
+    submit_button = QPushButton("Generate System Message")
     submit_button.clicked.connect(process_input)  # Connect button to function
     layout.addWidget(submit_button)
 
+    output_label = QLabel("System Message:")
+    layout.addWidget(output_label)
+
     output_area = QTextEdit()
     layout.addWidget(output_area)
+
+    status_bar = QStatusBar()
+    layout.addWidget(status_bar)
 
     window.setLayout(layout)
     window.show()
